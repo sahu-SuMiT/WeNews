a29a1974b1d3df828f5187178bde97fbac497ef5
SuMiT <sumitsahu1304@gmail.com>
2025-09-04 15:25:01 +0530

    implemented api integratios with the app

--- WeNews-master/android/app/src/main/kotlin/com/rishishwar/parichay/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.rishishwar.parichay
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- WeNews-master/lib/navigation/routes_constant.dart
+++
@@ -1,7 +0,0 @@
-class RoutesConstant {
-  static String signUpPage="/sign_up_page";
-  static String splashPage="/splash_page";
-  static String request="/sign_up_page";
-  static String loginPage = "/login_page";
-  static String homePage="/home_page";
-}
